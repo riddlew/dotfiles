cd62b06db082b1ec4e1e605dd81f8d2d6067b414
Wyatt (dleprog) <dleprogswe@gmail.com>
2025-03-04 17:54:29 -0500

    Change font size and font to Maple Mono for Wezterm

--- mac/wezterm/.config/wezterm/wezterm.lua
+++ mac/wezterm/.config/wezterm/wezterm.lua
@@ -86,13 +86,13 @@ config.colors = {
 -- config.default_prog = { "/usr/bin/zsh", "-l" }
 
 config.font = wezterm.font_with_fallback {
-	'JetBrainsMono Nerd Font',
+	'Maple Mono Normal NL NF',
 	-- 'SFMono Nerd Font',
 	-- 'IosevkaMineNew Nerd Font Extended'
 	-- 'InputMono NF'
 	-- 'InputMine 1.3 Nerd Font'
 }
-config.font_size = 13.5
+config.font_size = 15.0
 -- config.font_size = 10.0
 -- Using this because 1.4 is just a little too big for terminal use
 config.line_height = 1.2
