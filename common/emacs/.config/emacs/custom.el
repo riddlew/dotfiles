f31e54d5573a535699e610b654bdb097720ddd06
W. Riddle <dleprogswe@gmail.com>
2026-04-22 18:28:00 -0400

    Update emacs config
    - Automatically follow sym-links instead of prompting for yes/no.
    - Add `markdown-mode`, `evil-markdown`, and `markdown-indent-mode`.
    - Fix global keys to override other keymaps.

--- common/emacs/.config/emacs/custom.el
+++ common/emacs/.config/emacs/custom.el
@@ -8,11 +8,13 @@
  '(ns-command-modifier 'meta)
  '(ns-right-alternate-modifier 'super)
  '(package-selected-packages
-   '(ef-themes evil-collection evil-commentary exec-path-from-shell
-	       expand-region expreg general hydra magit mixed-pitch
-	       nerd-icons org-appear org-contrib org-modern sudo-edit
-	       toc-org ultra-scroll))
- '(show-trailing-whitespace t))
+   '(ef-themes evil-collection evil-commentary evil-markdown
+	       exec-path-from-shell expand-region expreg general
+	       grip-mode hydra magit markdown-appear
+	       markdown-indent-mode markdown-mode mixed-pitch
+	       nerd-icons org-appear org-contrib org-modern
+	       smartparens sudo-edit toc-org ultra-scroll))
+ '(show-trailing-whitespace nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
